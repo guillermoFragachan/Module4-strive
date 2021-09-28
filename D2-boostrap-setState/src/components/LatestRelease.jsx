3a02345b9038854f7bba16711979e1aad31da792
guillermoFragachan <guillefragachan@gmail.com>
2021-09-28 15:23:55 +0200

    homework done

--- D2-boostrap-setState/src/components/LatestRelease.jsx
+++ D2-boostrap-setState/src/components/LatestRelease.jsx
@@ -1,20 +1,47 @@
+import { Component } from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+
 import HistoryBooks from '../data/history.json'
 
-const LatestRelease = (props) =>(
-    <Container>
-        <Row>
-        <Col>
-        
 
-        
-        </Col>
-        </Row>
-    </Container>
+class LatestRelease extends Component {
+    state = {
 
-)
+    }
+
+
+
+    render() {
+        return(
+        <Container>
+            <Row>
+                
+           
+                            {
+                                HistoryBooks.map(book => (
+                                    <Col >
+                                        <img
+                                            className="d-block w-100"
+                                            src={book.img}
+                                            alt="First slide"
+                                            
+                                        />
+                                        </Col>
+                                       
+                                   
+                                ))
+                            }
+                       
+
+
+                
+            </Row>
+        </Container>
+    )
+    }
+}
 
 
 
