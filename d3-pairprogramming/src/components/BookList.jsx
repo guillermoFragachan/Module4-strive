efe398771e3c09584a332ae9de9d5c48f1e7f964
guillermoFragachan <guillefragachan@gmail.com>
2021-10-05 14:10:27 +0200

    change class to hooks

--- d3-pairprogramming/src/components/BookList.jsx
+++ d3-pairprogramming/src/components/BookList.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -9,44 +11,52 @@ import FantasyBooks from "../data/fantasy.json";
 
 import FormControl from "react-bootstrap/FormControl";
 
+const BookList =() => {
+  // state = {
+  //   title: "",
+  //   filteredBooks: FantasyBooks,
+  // };
 
-class BookList extends Component {
-  state = {
-    title: "",
-    filteredBooks: FantasyBooks,
-  };
+  const [title, setTitle] = useState('')
+  const [filteredBooks, setFilteredBooks] = useState(FantasyBooks)
 
-  filtreBooksList = (query) => {
+  const filtreBooksList=  (query) => {
     let filteredBooks = FantasyBooks.filter((book) =>
       book.title.toLowerCase().includes(query.toLowerCase())
     );
 
-    this.setState({ filteredBooks });
+    // this.setState({ filteredBooks });
+    setFilteredBooks(filteredBooks)
   };
 
-  render() {
+
+
+
+  // render() {
     return (
       <Container>
         <Row>
           <FormControl
             placeholder="Book Title"
             aria-label="Book Title"
-            value={this.state.title}
+            // value={this.state.title}
             onChange={
               // this will happen every time I input a keystroke
               (e) => {
-                this.setState({
-                  // with the ... your making a copy of all the properties
-                  // already existing into this.state.reservation
-                  title: e.target.value,
-                  // and then you're just overwriting ONE property
-                });
-                this.filtreBooksList(e.target.value)
+                setTitle(e.target.value)
+                // this.setState({
+                //   // with the ... your making a copy of all the properties
+                //   // already existing into this.state.reservation
+                //   title: e.target.value,
+                //   // and then you're just overwriting ONE property
+                // });
+                // this.filtreBooksList(e.target.value)
+                setFilteredBooks(e.target.value)
               }
             }
           />
 
-          {this.state.filteredBooks.map((book) => (
+          {filteredBooks.map((book) => (
             <Col key={book.asin} xs={2}>
               <MyCard book={book} />
             </Col>
@@ -54,7 +64,7 @@ class BookList extends Component {
         </Row>
       </Container>
     );
-  }
+  // }
 }
 
 export default BookList;
